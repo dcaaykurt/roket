c373112a0d343e365f9f341f3194d9861941e368
unknown <omerkurt099@gmail.com>
2020-06-29 22:58:32 +0300

    kalman

---
+++ oneDimensionalKalman.cpp
@@ -0,0 +1,19 @@
+#include "oneDimensionalKalman.h"
+
+const float R = 0.1, Q = 0.1;
+float KK = 0, xKOld = 0, xKNew = 0, pKOld = 1, pKNew = 0;
+
+float oneDimensionalKalman(int val){
+	// Time update
+	xKNew = xKOld;
+	pKNew = pKOld + Q;
+
+	//Measurment update
+	KK = pKNew / (pKNew + R);
+	xKNew = xKNew + KK * (val - xKOld);
+	pKNew = (1 - KK) * pKOld;
+	xKOld = xKNew;
+	pKOld = pKNew;
+	
+	return xKNew;
+}
---
+++ oneDimensionalKalman.h
@@ -0,0 +1,2 @@
+float oneDimensionalKalman(int);
+
